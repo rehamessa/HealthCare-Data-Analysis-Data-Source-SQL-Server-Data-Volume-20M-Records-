15306133a7d7c7581a95d037d9813e64988abde1
reham.essa <rehamessa96@gmail.com>
2025-11-08 08:47:46 +0200

    Create a  table for storing clinical data

--- create & populate.sql
+++ create & populate.sql
@@ -3,3 +3,13 @@ create database healthcare
 
 use healthcare
 
+-- Create a  table for storing clinical data
+CREATE TABLE ClinicalData (
+    HospitalID INT,
+    AdmissionDate DATE,
+    TotalAdmissions INT,
+    Readmissions INT,
+    Infections INT,
+    TotalDeaths INT,
+    AverageLengthOfStay DECIMAL(4,2)
+);
